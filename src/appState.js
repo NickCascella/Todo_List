aeb50d087377d82559be9655162b195f725ffbda
WhatAmIDoingWrong98 <83091530+WhatAmIDoingWrong98@users.noreply.github.com>
2021-08-02 22:51:23 -0400

    Add files via upload

--- src/appState.js
+++ src/appState.js
@@ -1,59 +1,51 @@
-
-
 class AppState {
-    constructor() {
-        this.projects = [];
+  constructor() {
+    this.projects = [];
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+  }
+
+  removeProject(project) {
+    let index = this.projects.indexOf(project);
+    if (index > -1) {
+      this.projects.splice(index, 1);
     }
+  }
 
-    addProject(project) {
-        this.projects.push(project);
+  hideAll() {
+    for (let i = 0; i < this.projects.length; i++) {
+      let project = this.projects[i];
+      project.hide();
     }
+  }
 
-    removeProject(project){
-        let index = this.projects.indexOf(project)
-        if(index > -1){
-            this.projects.splice(index,1)
-        }
-    }
-
-    hideAll() {
-        for (let i = 0; i < this.projects.length; i++) {
-            let project = this.projects[i];
-            project.hide();
-        }
-    }
-
-    displayDefault(){
-        
-        this.projects[0].show()
-
-    }
-
+  displayDefault() {
+    this.projects[0].show();
+  }
 }
 
 class Project {
-    constructor(name, selectElement, tasksElement) {
-        this.name = name;
-        this.tasks = [];
-        this.selectElement = selectElement;
-        this.tasksElement = tasksElement;
-    }
+  constructor(name, selectElement, tasksElement) {
+    this.name = name;
+    this.tasks = [];
+    this.selectElement = selectElement;
+    this.tasksElement = tasksElement;
+  }
 
-    remove(){
-        this.tasksElement.remove()
-    }
+  remove() {
+    this.tasksElement.remove();
+  }
 
+  show() {
+    this.tasksElement.style.display = "flex";
+  }
 
-    show() {
-        this.tasksElement.style.display = 'flex';
-    }
-
-    hide() {
-        this.tasksElement.style.display = 'none';
-    }
+  hide() {
+    this.tasksElement.style.display = "none";
+  }
 }
-
 
 export { Project };
 export default new AppState();
-
--- src/todo_Main.js
+++ src/todo_Main.js
@@ -1,135 +1,75 @@
-import {createOtherTasks} from './other'
-import appState, { Project } from './appState';
-import './todo.css';
+import { createOtherTasks } from "./other";
+import appState, { Project } from "./appState";
+import "./todo.css";
 
+let newSelection = document.getElementById("selection");
+let newProjectBtn = document.createElement("button");
+newProjectBtn.classList = "mainButtons";
+newProjectBtn.textContent = "Add Project +";
+let projectOrder = true;
+let newProjectDiv = document.createElement("div");
+newProjectDiv.id = "projectFolder";
+newProjectDiv.classList = "newProjectDiv";
 
+newProjectBtn.addEventListener("click", function createProj() {
+  if (projectOrder === true) {
+    projectOrder = false;
+    let projectTitle = document.createElement("input");
+    projectTitle.setAttribute("type", "text");
+    projectTitle.maxLength = "13";
+    projectTitle.style.marginTop = "30px";
+    projectTitle.addEventListener("keypress", function create(e) {
+      if (e.key === "Enter") {
+        appState.hideAll();
+        let enteredTitle = document.createElement("button");
+        enteredTitle.textContent = projectTitle.value;
+        enteredTitle.classList = "mainButtons";
+        newProjectDiv.appendChild(enteredTitle);
+        let deleteProject = document.createElement("button");
+        deleteProject.textContent = "X";
+        deleteProject.classList = "deleteProj";
+        deleteProject.addEventListener("click", function deleteInstance() {
+          project.remove();
+          appState.removeProject(project);
+          newProjectDiv.removeChild(enteredTitle);
+          newProjectDiv.removeChild(deleteProject);
+          appState.hideAll();
+          appState.displayDefault();
+        });
+        newProjectDiv.appendChild(deleteProject);
+        newProjectDiv.removeChild(projectTitle);
+        projectOrder = true;
+        let tasksElement = createOtherTasks();
 
-let newSelection = document.getElementById('selection')
-let newProjectBtn = document.createElement('button')
-newProjectBtn.classList = 'mainButtons'
-newProjectBtn.textContent = 'Add Project +'
-let projectOrder = true
-let newProjectDiv = document.createElement('div')
-newProjectDiv.id = 'projectFolder'
-newProjectDiv.classList = 'newProjectDiv'
+        let project = new Project(
+          projectTitle.value,
+          enteredTitle,
+          tasksElement
+        );
+        appState.addProject(project);
 
-newProjectBtn.addEventListener('click', function createProj() {
-    if(projectOrder === true){
-    projectOrder = false
-    let projectTitle = document.createElement('input')
-    projectTitle.setAttribute('type', 'text')
-    projectTitle.maxLength = '13'
-    projectTitle.style.marginTop = '30px'
-    projectTitle.addEventListener('keypress', function create(e){
-        if(e.key === 'Enter'){
-            appState.hideAll();
-            let enteredTitle = document.createElement('button')
-            enteredTitle.textContent = projectTitle.value
-            enteredTitle.classList = 'mainButtons'
-            newProjectDiv.appendChild(enteredTitle)
-            let deleteProject = document.createElement('button')
-            deleteProject.textContent = 'X'
-            deleteProject.classList = 'deleteProj'
-            deleteProject.addEventListener('click', function deleteInstance(){
-                project.remove()
-                appState.removeProject(project)
-                newProjectDiv.removeChild(enteredTitle)
-                newProjectDiv.removeChild(deleteProject)
-                appState.hideAll()
-                appState.displayDefault()
-            })
-            newProjectDiv.appendChild(deleteProject)
-            newProjectDiv.removeChild(projectTitle)
-            projectOrder = true
-            let tasksElement = createOtherTasks();
-
-            let project = new Project(projectTitle.value, enteredTitle, tasksElement);
-            appState.addProject(project);
-
-            enteredTitle.addEventListener('click', function selectMe(){
-                appState.hideAll();
-                project.show();
-            })
-        }
-    })
-    newProjectDiv.appendChild(projectTitle)
-}
-})
-
-
-//creating general button
-let generalBtn = document.createElement('button')
-generalBtn.classList = 'mainButtons'
-generalBtn.textContent = 'General'
-let tasksElement = createOtherTasks();
-let project = new Project('General', generalBtn, tasksElement);
-appState.addProject(project);
-generalBtn.addEventListener('click', function selectMe(){
-    appState.hideAll();
-    project.show();
-})
-
-
-newSelection.appendChild(generalBtn)
-newSelection.appendChild(newProjectBtn)
-newSelection.appendChild(newProjectDiv)
-
-/*
-let newSelection = document.getElementById('selection')
-let newProjectBtn = document.createElement('button')
-newProjectBtn.classList = 'mainButtons'
-newProjectBtn.textContent = 'Add Project +'
-let projectOrder = true
-let newProjectDiv = document.createElement('div')
-newProjectDiv.id = 'projectFolder'
-
-newProjectBtn.addEventListener('click', function createProj() {
-    if(projectOrder === true){
-    projectOrder = false
-    let projectTitle = document.createElement('input')
-    projectTitle.setAttribute('type', 'text')
-    projectTitle.style.marginTop = '30px'
-    projectTitle.addEventListener('keypress', function create(e){
-        if(e.key === 'Enter'){
-            appState.hideAll();
-            let enteredTitle = document.createElement('button')
-            enteredTitle.textContent = projectTitle.value
-            enteredTitle.classList = 'mainButtons'
-            newProjectDiv.appendChild(enteredTitle)
-            newProjectDiv.removeChild(projectTitle)
-            projectOrder = true
-            let tasksElement = createOtherTasks();
-
-            let project = new Project(projectTitle.value, enteredTitle, tasksElement);
-            appState.addProject(project);
-
-            enteredTitle.addEventListener('click', function selectMe(){
-                appState.hideAll();
-                project.show();
-            })
-        }
-    })
-    newProjectDiv.appendChild(projectTitle)
-}
-})
-
+        enteredTitle.addEventListener("click", function selectMe() {
+          appState.hideAll();
+          project.show();
+        });
+      }
+    });
+    newProjectDiv.appendChild(projectTitle);
+  }
+});
 
 //creating general button
-let generalBtn = document.createElement('button')
-generalBtn.classList = 'mainButtons'
-generalBtn.textContent = 'General'
+let generalBtn = document.createElement("button");
+generalBtn.classList = "mainButtons";
+generalBtn.textContent = "General";
 let tasksElement = createOtherTasks();
-let project = new Project('General', generalBtn, tasksElement);
+let project = new Project("General", generalBtn, tasksElement);
 appState.addProject(project);
-generalBtn.addEventListener('click', function selectMe(){
-    appState.hideAll();
-    project.show();
-})
+generalBtn.addEventListener("click", function selectMe() {
+  appState.hideAll();
+  project.show();
+});
 
-
-newSelection.appendChild(generalBtn)
-newSelection.appendChild(newProjectBtn)
-newSelection.appendChild(newProjectDiv)
-
-
-*/
+newSelection.appendChild(generalBtn);
+newSelection.appendChild(newProjectBtn);
+newSelection.appendChild(newProjectDiv);
